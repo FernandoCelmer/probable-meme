4dbc4fb4b161fcdad1d0b5a59f57366b4900ff9c
FernandoCelmer <email@fernandocelmer.com>
2024-02-25 02:51:50 -0300

    ⚙️ FEATURE: API

---
+++ src/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	web "github.com/FernandoCelmer/api.go/src"
+)
+
+func main() {
+	app := web.NewApp(
+		web.Title("API"),
+		web.Description("Probable Meme"),
+		web.Version("0.1.0"),
+	)
+
+	app.Get("/item", itemHandler)
+
+	app.Run(web.Port(8080))
+}
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func itemHandler(w http.ResponseWriter, r *http.Request) {
+	data := Response{Message: "Item"}
+	response, _ := json.Marshal(data)
+	fmt.Fprintf(w, string(response))
+}
